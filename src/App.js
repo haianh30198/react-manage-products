6299bf25d14a42b393347ccbf82ddfd35d6ca7aa
Nguyễn Hải Anh <haianh30198@gmail.com>
2021-08-26 22:44:27 +0700

    Bai 88 | Tich Hop React-Router P1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,49 @@ import React, { Component } from "react";
 import "./App.css";
 import Menu from "./components/Menu/Menu";
 import ProductList from "./components/ProductList/ProductList";
+import routes from "./routes";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Menu />
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <Router>
+                <div>
+                    <Menu />
+                    <div className="container">
+                        <div className="row">
+                            {/* <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             <button type="button" className="btn btn-info mb-3">
                                 Add New
                             </button>
                             <ProductList />
+                        </div> */}
+                            {this.showContent(routes)}
                         </div>
                     </div>
                 </div>
-            </div>
+            </Router>
         );
     }
+
+    showContent = (routes) => {
+        var result = null;
+
+        if (routes.length > 0) {
+            result = routes.map((route, index) => {
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        component={route.main}
+                    />
+                );
+            });
+        }
+
+        return <Switch>{result}</Switch>;
+    };
 }
 
 export default App;
